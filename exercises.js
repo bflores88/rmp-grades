187884d9aed6b80b5d9508f86f112fb8d0879345
kmietotwy <kmietotwy@gmail.com>
2019-03-20 20:06:03 -1000

    forgot 2 push

--- exercises.js
+++ exercises.js
@@ -15,4 +15,102 @@
 // 9. generates a new score list with a 5% increase for each score 
 // 10.generates a list of scores that are odd numbered
 
+var allScores = (function(){
+    var scores = [65, 76, 100, 88, 17, 55];
 
+    var sumScores = 0;
+    var avgScores = 0;
+    var highestScore = 0;
+
+    var sum = function(){
+        for(var i=0; i<scores.length; i++){
+            sumScores+= scores[i];
+        }
+        return sumScores;
+    }
+
+    var avg = function(){
+        avgScores = sumScores / scores.length;
+        return avgScores;
+    }
+
+    var highest = function(){
+        scores.forEach(function(elem){
+            if(elem > highestScore){
+                highestScore = elem;
+            }
+        })
+        return highestScore;
+    }
+
+    var lowestScore = 100;
+
+    var lowest = function(){
+        scores.forEach(function(elem){
+            if(elem < lowestScore){
+                lowestScore = elem;
+            }
+        })
+        return lowestScore;
+    }
+
+    var random = function(){
+        var randomScore = scores[Math.floor(Math.random()*scores.length)];
+        return randomScore;
+    }
+
+    var first = function(){
+        var firstScore = scores[0];
+        return firstScore;
+    }
+
+    var last = function(){
+        var lastScore = scores[scores.length - 1];
+        return lastScore;
+    }
+
+    var increaseBy5 = function(){
+        var newScores = [];
+        for(var i=0; i<scores.length; i++){
+            var increase = scores[i] * 1.05;
+            newScores.push(increase);
+        }
+        return newScores;
+    }
+
+    var odd = function(){
+        var oddScores = [];
+        for(var i=0; i<scores.length; i++){
+            if(scores[i]%2 === 1){
+                oddScores.push(scores[i]);
+            }
+        }
+        return oddScores;
+    }
+
+    return {
+        sum: sum,
+        average: avg,
+        highest: highest,
+        lowest: lowest,
+        random: random,
+        first: first,
+        last: last,
+        increase: increaseBy5,
+        odd: odd
+    }
+
+
+
+}())
+
+
+console.log(allScores.sum());
+console.log(allScores.average());
+console.log(allScores.highest());
+console.log(allScores.lowest());
+console.log(allScores.random());
+console.log(allScores.first());
+console.log(allScores.last());
+console.log(allScores.increase());
+console.log(allScores.odd());
